d2164a826c22c286ef677c58e406a1d88c6ff1a7
Aleksandar Ristic <storiaca@gmail.com>
2023-10-09 09:35:01 +0200

    Main script setup

--- vanilla-js-not-framework/coffeemasters/app.js
+++ vanilla-js-not-framework/coffeemasters/app.js
@@ -1,1 +1,9 @@
-console.log('app');
+// It's better to wait for the event for manipulation
+window.addEventListener('DOMContentLoaded', () => {
+  let nav = document.querySelector('nav');
+  console.log(nav);
+  nav.innerHTML = `
+    <h2>Hello HTML</h2>
+    <p>This is an HTML within javascript string</p>
+  `;
+});
