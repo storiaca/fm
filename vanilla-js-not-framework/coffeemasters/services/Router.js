5864205245f456e75daf8d4e3982fdb630a65c6e
Aleksandar Ristic <storiaca@gmail.com>
2023-10-11 11:28:35 +0200

    Changing DOM element content

--- vanilla-js-not-framework/coffeemasters/services/Router.js
+++ vanilla-js-not-framework/coffeemasters/services/Router.js
@@ -16,6 +16,30 @@ const Router = {
     if (addToHistory) {
       history.pushState({ route }, null, route);
     }
+    let pageElement = null;
+
+    switch (route) {
+      case '/':
+        pageElement = document.createElement('h1');
+        pageElement.textContent = 'Menu';
+        break;
+      case '/order':
+        pageElement = document.createElement('h1');
+        pageElement.textContent = 'Your Order';
+        break;
+
+      default:
+        break;
+    }
+
+    if (pageElement) {
+      ///document.querySelector('main').children[0].remove();
+      const cache = document.querySelector('main');
+      cache.innerHTML = '';
+      cache.appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    }
   },
 };
 
