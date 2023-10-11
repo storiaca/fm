cd212e5b5e91b396a5b7bd156f9dddb48b523223
Aleksandar Ristic <storiaca@gmail.com>
2023-10-11 17:03:39 +0200

    Loading templates

--- vanilla-js-not-framework/coffeemasters/components/MenuPage.js
+++ vanilla-js-not-framework/coffeemasters/components/MenuPage.js
@@ -2,6 +2,13 @@ export class MenuPage extends HTMLElement {
   constructor() {
     super();
   }
+
+  // whem component is attached to the DOM
+  connectedCallback() {
+    const template = document.getElementById('menu-page-template');
+    const content = template.content.cloneNode(true);
+    this.appendChild(content);
+  }
 }
 
 customElements.define('menu-page', MenuPage);
--- vanilla-js-not-framework/coffeemasters/services/Router.js
+++ vanilla-js-not-framework/coffeemasters/services/Router.js
@@ -26,17 +26,14 @@ const Router = {
     switch (route) {
       case '/':
         pageElement = document.createElement('menu-page');
-        pageElement.textContent = 'Menu';
         break;
       case '/order':
         pageElement = document.createElement('order-page');
-        pageElement.textContent = 'Your Order';
         break;
 
       default:
         if (route.startsWith('/product-')) {
           pageElement = document.createElement('details-page');
-          pageElement.textContent = 'Details';
           const paramId = route.substring(route.lastIndexOf('-') + 1);
           pageElement.dataset.id = paramId;
         }
