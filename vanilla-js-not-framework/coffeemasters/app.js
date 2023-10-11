1bd4f1ec09c8e3b99b7cd398782e1f13a919fe2e
Aleksandar Ristic <storiaca@gmail.com>
2023-10-11 08:52:23 +0200

    SPA Router from scratch

--- vanilla-js-not-framework/coffeemasters/app.js
+++ vanilla-js-not-framework/coffeemasters/app.js
@@ -1,12 +1,15 @@
 import Store from './services/Store.js';
 import API from './services/API.js';
 import { loadData } from './services/Menu.js';
+import Router from './services/Router.js';
 
 window.app = {};
 
 app.store = Store;
+app.router = Router;
 
 // It's better to wait for the event for manipulation
 window.addEventListener('DOMContentLoaded', async () => {
   loadData();
+  app.router.init();
 });
---
+++ vanilla-js-not-framework/coffeemasters/services/Router.js
@@ -0,0 +1,22 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll('a.navlink').forEach((a) => {
+      a.addEventListener('click', (event) => {
+        event.preventDefault();
+        //const url1 = event.target.href; // full url
+        const url = event.target.getAttribute('href');
+        Router.go(url);
+      });
+    });
+    // Check the initial URL
+    Router.go(location.pathname);
+  },
+  go: (route, addToHistory = true) => {
+    console.log(`Going to ${route}`);
+    if (addToHistory) {
+      history.pushState({ route }, null, route);
+    }
+  },
+};
+
+export default Router;
