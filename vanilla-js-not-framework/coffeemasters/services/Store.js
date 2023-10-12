ca420a473c9f8fa8bb989ca4f37fcb0acb693d42
Aleksandar Ristic <storiaca@gmail.com>
2023-10-12 10:20:16 +0200

    Creating a Proxy

--- vanilla-js-not-framework/coffeemasters/services/Store.js
+++ vanilla-js-not-framework/coffeemasters/services/Store.js
@@ -3,4 +3,19 @@ const Store = {
   cart: [],
 };
 
-export default Store;
+const proxiedStore = new Proxy(Store, {
+  set(target, property, value) {
+    target[property] = value;
+    if (property == 'menu') {
+      window.dispatchEvent(new Event('appmenuchange'));
+    }
+
+    if (property == 'cart') {
+      window.dispatchEvent(new Event('appcarthange'));
+    }
+
+    return true;
+  },
+});
+
+export default proxiedStore;
