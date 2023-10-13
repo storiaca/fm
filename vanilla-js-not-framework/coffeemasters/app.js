d0bada826b6e92cc004a649294d6a850b429cdce
Aleksandar Ristic <storiaca@gmail.com>
2023-10-13 11:51:08 +0200

    Binding form data

--- vanilla-js-not-framework/coffeemasters/app.js
+++ vanilla-js-not-framework/coffeemasters/app.js
@@ -8,6 +8,8 @@ import { MenuPage } from './components/MenuPage.js';
 import { DetailsPage } from './components/DetailsPage.js';
 import { OrderPage } from './components/OrderPage.js';
 import ProductItem from './components/ProductItem.js';
+import CartItem from './components/CartItem.js';
+
 window.app = {};
 
 app.store = Store;
@@ -21,7 +23,9 @@ window.addEventListener('DOMContentLoaded', async () => {
 
 window.addEventListener("appcartchange", event => {
   const badge = document.getElementById("badge");
-  const qty = app.store.cart.reduce((acc, item) => acc + item.qunatity, 0)
+  const qty = app.store.cart.reduce(
+      (acc, item) => acc + item.quantity, 0
+  );
   badge.textContent = qty;
   badge.hidden = qty == 0;
 })
--- vanilla-js-not-framework/coffeemasters/services/Router.js
+++ vanilla-js-not-framework/coffeemasters/services/Router.js
@@ -35,7 +35,7 @@ const Router = {
         if (route.startsWith('/product-')) {
           pageElement = document.createElement('details-page');
           const paramId = route.substring(route.lastIndexOf('-') + 1);
-          pageElement.dataset.productId = paramId;
+          pageElement.dataset.id = paramId;
         }
         break;
     }
