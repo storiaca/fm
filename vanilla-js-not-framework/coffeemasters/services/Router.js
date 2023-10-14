e7421e5e8737f620c0d42fb89ee798e3cd501cd0
Aleksandar Ristic <storiaca@gmail.com>
2023-10-14 11:28:50 +0200

    404 redicrection

--- vanilla-js-not-framework/coffeemasters/services/Router.js
+++ vanilla-js-not-framework/coffeemasters/services/Router.js
@@ -1,16 +1,16 @@
 const Router = {
   init: () => {
-    document.querySelectorAll('a.navlink').forEach((a) => {
-      a.addEventListener('click', (event) => {
+    document.querySelectorAll("a.navlink").forEach((a) => {
+      a.addEventListener("click", (event) => {
         event.preventDefault();
         //const url1 = event.target.href; // full url
-        const url = event.target.getAttribute('href');
+        const url = event.target.getAttribute("href");
         Router.go(url);
       });
     });
 
     // Event Handler for URL changes
-    window.addEventListener('popstate', (event) => {
+    window.addEventListener("popstate", (event) => {
       Router.go(event.state.route, false);
     });
     // Check the initial URL
@@ -24,17 +24,17 @@ const Router = {
     let pageElement = null;
 
     switch (route) {
-      case '/':
-        pageElement = document.createElement('menu-page');
+      case "/":
+        pageElement = document.createElement("menu-page");
         break;
-      case '/order':
-        pageElement = document.createElement('order-page');
+      case "/order":
+        pageElement = document.createElement("order-page");
         break;
 
       default:
-        if (route.startsWith('/product-')) {
-          pageElement = document.createElement('details-page');
-          const paramId = route.substring(route.lastIndexOf('-') + 1);
+        if (route.startsWith("/product-")) {
+          pageElement = document.createElement("details-page");
+          const paramId = route.substring(route.lastIndexOf("-") + 1);
           pageElement.dataset.id = paramId;
         }
         break;
@@ -42,11 +42,13 @@ const Router = {
 
     if (pageElement) {
       ///document.querySelector('main').children[0].remove();
-      const cache = document.querySelector('main');
-      cache.innerHTML = '';
+      const cache = document.querySelector("main");
+      cache.innerHTML = "";
       cache.appendChild(pageElement);
       window.scrollX = 0;
       window.scrollY = 0;
+    } else {
+      document.querySelector("main").innerHTML = "Oups, 404!";
     }
   },
 };
