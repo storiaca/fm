3e67c7a0766952bc61da7c7c9acb908a28b46083
Aleksandar Ristic <storiaca@gmail.com>
2023-10-14 11:13:41 +0200

    Binding form data

--- vanilla-js-not-framework/coffeemasters/components/MenuPage.js
+++ vanilla-js-not-framework/coffeemasters/components/MenuPage.js
@@ -2,13 +2,13 @@ export class MenuPage extends HTMLElement {
   constructor() {
     super();
 
-    this.root = this.attachShadow({ mode: 'open' });
+    this.root = this.attachShadow({ mode: "open" });
 
-    const styles = document.createElement('style');
+    const styles = document.createElement("style");
     this.root.appendChild(styles);
 
     async function loadCSS() {
-      const request = await fetch('/components/MenuPage.css');
+      const request = await fetch("/components/MenuPage.css");
       const css = await request.text();
       styles.textContent = css;
     }
@@ -18,37 +18,38 @@ export class MenuPage extends HTMLElement {
 
   // when component is attached to the DOM
   connectedCallback() {
-    const template = document.getElementById('menu-page-template');
+    const template = document.getElementById("menu-page-template");
     const content = template.content.cloneNode(true);
     this.root.appendChild(content);
 
-    window.addEventListener('appmenuchange', () => {
+    window.addEventListener("appmenuchange", () => {
       this.render();
     });
+    this.render();
   }
 
   render() {
     if (app.store.menu) {
-      this.root.querySelector('#menu').innerHTML = '';
+      this.root.querySelector("#menu").innerHTML = "";
       for (let category of app.store.menu) {
-        const liCategory = document.createElement('li');
+        const liCategory = document.createElement("li");
         liCategory.innerHTML = `
           <h3>${category.name}</h3>
           <ul class="category">
           </ul>
         `;
-        this.root.querySelector('#menu').appendChild(liCategory);
+        this.root.querySelector("#menu").appendChild(liCategory);
 
         category.products.forEach((product) => {
-          const item = document.createElement('product-item');
+          const item = document.createElement("product-item");
           item.dataset.product = JSON.stringify(product);
-          liCategory.querySelector('ul').appendChild(item);
+          liCategory.querySelector("ul").appendChild(item);
         });
       }
     } else {
-      this.root.querySelector('#menu').innerHTML = 'Loading...';
+      this.root.querySelector("#menu").innerHTML = "Loading...";
     }
   }
 }
 
-customElements.define('menu-page', MenuPage);
+customElements.define("menu-page", MenuPage);
