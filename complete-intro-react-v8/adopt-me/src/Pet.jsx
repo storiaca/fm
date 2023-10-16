5f0ebca41512e123c2e6880a33dab99efc18f11b
Aleksandar Ristic <storiaca@gmail.com>
2023-10-16 08:54:48 +0200

    Styling the Pet component

--- complete-intro-react-v8/adopt-me/src/Pet.jsx
+++ complete-intro-react-v8/adopt-me/src/Pet.jsx
@@ -1,9 +1,23 @@
-const Pet = (props) => {
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
   return (
     <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
+      <a href={`/details/${id}`} className="pet">
+        <div className="image-container">
+          <picture>
+            <img src={hero} alt={name} />
+          </picture>
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+        </div>
+      </a>
     </div>
   );
 };
