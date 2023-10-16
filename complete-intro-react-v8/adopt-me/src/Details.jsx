3b37bcd3bcaa9ed64eaff6169c1d38ee39c89ea5
Aleksandar Ristic <storiaca@gmail.com>
2023-10-16 11:08:37 +0200

    useParams

--- complete-intro-react-v8/adopt-me/src/Details.jsx
+++ complete-intro-react-v8/adopt-me/src/Details.jsx
@@ -1,7 +1,9 @@
+import { useParams } from "react-router-dom";
 function Details() {
+  const { id } = useParams();
   return (
     <div>
-      <h2>Details</h2>
+      <h2>{id}</h2>
     </div>
   );
 }
