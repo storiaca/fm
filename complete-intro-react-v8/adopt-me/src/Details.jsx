f6086f11c6d4e1eb42ea45f223dfe66789ed906b
Aleksandar Ristic <storiaca@gmail.com>
2023-10-16 14:10:08 +0200

    Performance optimization with react query

--- complete-intro-react-v8/adopt-me/src/Details.jsx
+++ complete-intro-react-v8/adopt-me/src/Details.jsx
@@ -1,9 +1,34 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
+
 function Details() {
   const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet);
+
+  if (results.isError) {
+    return <h2>Error</h2>;
+  }
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">Loading...</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
   return (
-    <div>
-      <h2>{id}</h2>
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+          <button>Adopt {pet.name}</button>
+          <p>{pet.description}</p>
+        </h2>
+      </div>
     </div>
   );
 }
