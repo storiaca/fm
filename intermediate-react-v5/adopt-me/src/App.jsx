0616316e95214bb40e3b8b397024e443b5a7f4d1
Aleksandar Ristic <storiaca@gmail.com>
2023-10-19 12:31:35 +0200

    Styling with tailwind css

--- intermediate-react-v5/adopt-me/src/App.jsx
+++ intermediate-react-v5/adopt-me/src/App.jsx
@@ -26,8 +26,10 @@ const App = () => {
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
           <AdoptedPetContext.Provider value={adoptedPet}>
-            <header>
-              <Link to="/">Adopt Me!</Link>
+            <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                Adopt Me!
+              </Link>
             </header>
             <Routes>
               <Route path="/details/:id" element={<Details />} />
