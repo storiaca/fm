a58c95ce535583fcea559efdecf655b1192611fc
Aleksandar Ristic <storiaca@gmail.com>
2023-10-27 10:08:23 +0200

    Context API start

---
+++ react-performance/packing-list-context/src/components/application.js
@@ -0,0 +1,36 @@
+import { useReducer, useState } from 'react';
+import { filterItems, getInitialItems } from '../lib/items';
+import { reducer } from '../lib/reducer';
+import Header from './header';
+import ItemList from './item-list';
+import MarkAllAsUnpacked from './mark-all-as-unpacked';
+import NewItem from './new-item';
+
+const Application = () => {
+  const [items, setItems] = useState(() => getInitialItems());
+
+  const unpackedItems = filterItems(items, { packed: false });
+  const packedItems = filterItems(items, { packed: true });
+
+  return (
+    <main className="mx-auto flex flex-col gap-8 p-8 lg:max-w-4xl">
+      <Header items={items} />
+      <NewItem setItems={setItems} />
+      <section className="flex flex-col gap-8 md:flex-row">
+        <ItemList
+          title="Unpacked Items"
+          items={unpackedItems}
+          setItems={setItems}
+        />
+        <ItemList
+          title="Packed Items"
+          items={packedItems}
+          setItems={setItems}
+        />
+      </section>
+      <MarkAllAsUnpacked setItems={setItems} />
+    </main>
+  );
+};
+
+export default Application;
---
+++ react-performance/packing-list-context/src/components/item.js
@@ -0,0 +1,56 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+import { updateItem } from '../lib/items';
+import { remove, update } from '../lib/reducer';
+
+const Item = ({ item, setItems }) => {
+  const [editing, setEditing] = useState(false);
+
+  return (
+    <li className="flex items-center gap-2">
+      <input
+        type="checkbox"
+        className="focus:bg-red-500"
+        checked={item.packed}
+        id={`toggle-${item.id}`}
+        onChange={() =>
+          setItems((items) =>
+            updateItem(items, item.id, { packed: !item.packed }),
+          )
+        }
+      />
+      <label
+        htmlFor={`toggle-${item.id}`}
+        className={clsx({ hidden: editing })}
+      >
+        {item.name}
+      </label>
+      <input
+        value={item.name}
+        id={`edit-${item.id}`}
+        className={clsx('py-0 text-sm', { hidden: !editing })}
+        onChange={(event) =>
+          setItems(update(item.id, { name: event.target.value }))
+        }
+      />
+      <div className="flex gap-2">
+        <button
+          className="px-2 py-0 text-xs"
+          aria-label={`Edit "${item.name}"`}
+          onClick={() => setEditing(!editing)}
+        >
+          {editing ? '💾 Save' : '✍️ Edit'}
+        </button>
+        <button
+          className="px-2 py-0 text-xs"
+          aria-label={`Remove "${item.name}"`}
+          onClick={() => setItems(remove(item.id))}
+        >
+          🗑 Remove
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default Item;
