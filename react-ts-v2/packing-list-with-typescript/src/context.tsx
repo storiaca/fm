8d0ca884783d39feb3957b296147c3a02bdf0387
Aleksandar Ristic <storiaca@gmail.com>
2023-12-09 17:21:14 +0100

    Typescript utilities methods

--- react-ts-v2/packing-list-with-typescript/src/context.tsx
+++ react-ts-v2/packing-list-with-typescript/src/context.tsx
@@ -7,12 +7,15 @@ import {
   updateItem,
 } from './lib/items';
 
+type PartialItem = Partial<Item>;
+type WithoutId = Omit<PartialItem, 'id'>;
+
 type ItemsState = {
   items: Item[];
   unpackedItems: Item[];
   packedItems: Item[];
   add: (name: string) => void;
-  update: (id: string, updates: any) => void;
+  update: (id: string, updates: WithoutId) => void;
   remove: (id: string) => void;
   markAllAsUnpacked: () => void;
 };
@@ -27,7 +30,7 @@ const ItemsProvider = ({ children }: PropsWithChildren) => {
     setItems([...items, item]);
   };
 
-  const update = (id: string, updates: any) => {
+  const update = (id: string, updates: WithoutId) => {
     setItems(updateItem(items, id, updates));
   };
 
