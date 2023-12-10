b5bc1144a898895510567ac9e6507a675e5a6a3b
Aleksandar Ristic <storiaca@gmail.com>
2023-12-10 12:50:14 +0100

    Union types example

--- react-ts-v2/colors/src/global.d.ts
+++ react-ts-v2/colors/src/global.d.ts
@@ -1,1 +1,13 @@
 // export type AdjustColorActions = UpdateHexColorAction | UpdateRGBColorAction;
+
+type PropsWithHexColor<P> = P & {
+  hexColor: string;
+};
+
+type PropsWithDispatch<P> = P & {
+  dispatch: React.Dispatch<ColorActions>;
+};
+
+type PropsWithHexColorAndDispatch<P> = P &
+  PropsWithHexColor<P> &
+  PropsWithDispatch<P>;
