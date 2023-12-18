38563420f0a8b994f4a75db95e58623cf184d530
Aleksandar Ristic <storiaca@gmail.com>
2023-12-18 19:07:49 +0100

    Combine reducers finished

--- redux-fundamentals/examples/combine-reducers.js
+++ redux-fundamentals/examples/combine-reducers.js
@@ -26,18 +26,25 @@ const ADD_TASK = "ADD_TASK";
 const addTask = (title) => ({ type: ADD_TASK, payload: title });
 const addUser = (name) => ({ type: ADD_USER, payload: name });
 
-const reducer = (state = initialState, action) => {
+const userReducer = (users = initialState.users, action) => {
   if (action.type === ADD_USER) {
-    return {
-      ...state,
-      users: [...state.useres, action.payload],
-    };
+    return [...users, action.payload];
   }
-
-  if (action.type === ADD_TASK) {
-    return {
-      ...state,
-      taska: [...state.tasks, action.payload],
-    };
-  }
+  return users;
 };
+
+const taskReducer = (tasks = initialState.tasks, action) => {
+  if (action.type === ADD_TASK) {
+    return [...tasks, action.payload];
+  }
+  return tasks;
+};
+
+const reducer = combineReducers({
+  users: userReducer,
+  tasks: taskReducer,
+});
+
+const store = createStore(reducer);
+
+console.log(store.getState());
