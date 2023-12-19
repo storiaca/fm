f7f1b19c424551f8d68db8750384a2a172483faf
Aleksandar Ristic <storiaca@gmail.com>
2023-12-19 13:46:42 +0100

    Connecting state hooks to the store

--- redux-fundamentals/incident-counter/src/Counter.js
+++ redux-fundamentals/incident-counter/src/Counter.js
@@ -1,14 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { set } from './actions';
+
 export const Counter = () => {
   const incident = 'Incident';
-  const count = 0;
-
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
         <button>Increment</button>
-        <button>Reset</button>
+        <button onClick={() => dispatch(set(0))}>Reset</button>
         <button>Decrement</button>
       </section>
     </main>
