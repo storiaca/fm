3df4d943fc4490eee485e44c551fd1f366ee2f47
Aleksandar Ristic <storiaca@gmail.com>
2023-12-23 14:04:30 +0100

    Create humanSlice

--- redux-fundamentals/chores-redux/src/store/index.js
+++ redux-fundamentals/chores-redux/src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { humanSlice } from './humanSlice';
 import { tasksSlice } from './tasksSlice';
 
 export const store = configureStore({
   reducer: {
     tasks: tasksSlice.reducer,
-    humans: (state) => []
+    humans: humanSlice.reducer
   }
 });
