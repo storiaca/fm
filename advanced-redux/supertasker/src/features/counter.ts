df1f97f9df003e749ce92c727187d499f90f1d5b
Aleksandar Ristic <storiaca@gmail.com>
2023-12-28 09:36:04 +0100

    Create action and counter finished

--- advanced-redux/supertasker/src/features/counter.ts
+++ advanced-redux/supertasker/src/features/counter.ts
@@ -1,10 +1,20 @@
+import { createAction } from '@reduxjs/toolkit';
+
 type CounterState = { count: number };
 type CounterAction =
   | { type: 'INCREMENT' | 'DECREMENT'; payload: number }
   | { type: 'RESET' };
 
+const increment = createAction('INCREMENT', (amount: number) => {
+  return {
+    payload: amount,
+  };
+});
+
+const incrementAction = increment(5);
+
 export const reducer = (state: CounterState, action: CounterAction) => {
-  if (action.type === 'INCREMENT') {
+  if (action.type === increment.type) {
     return { count: state.count + action.payload };
   }
 
