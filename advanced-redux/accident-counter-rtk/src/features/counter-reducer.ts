3a27e39089ceebf7941af2830c929b43f5ff261a
Aleksandar Ristic <storiaca@gmail.com>
2023-12-28 09:54:45 +0100

    Create action exercise refactor

--- advanced-redux/accident-counter-rtk/src/features/counter-reducer.ts
+++ advanced-redux/accident-counter-rtk/src/features/counter-reducer.ts
@@ -5,16 +5,26 @@ type CounterAction =
   | { type: 'INCREMENT' | 'DECREMENT'; payload: number }
   | { type: 'RESET' };
 
+const increment = createAction('INCREMENT', (amount: number) => {
+  return { payload: amount };
+});
+
+const decrement = createAction('DECREMENT', (amount: number) => {
+  return { payload: amount };
+});
+
+const reset = createAction('RESET');
+
 export const reducer = (state: CounterState, action: CounterAction) => {
-  if (action.type === 'INCREMENT') {
+  if (action.type === increment.type) {
     return { count: state.count + action.payload };
   }
 
-  if (action.type === 'DECREMENT') {
+  if (action.type === decrement.type) {
     return { count: state.count - action.payload };
   }
 
-  if (action.type === 'RESET') {
+  if (action.type === reset.type) {
     return { count: 0 };
   }
 
