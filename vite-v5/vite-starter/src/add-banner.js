e2751b208217348712e3de32c002b8dcce6cbd2b
Aleksandar Ristic <storiaca@gmail.com>
2024-01-15 13:35:15 +0100

    Build CSS module exercise

--- vite-v5/vite-starter/src/add-banner.js
+++ vite-v5/vite-starter/src/add-banner.js
@@ -1,3 +1,7 @@
+import styles from './banner.module.css';
 export const addBanner = (text) => {
-  document.querySelector('#content').textContent = text;
+  const content = document.querySelector('#content');
+  content.textContent = text;
+
+  content.classList.add(styles.banner);
 };
