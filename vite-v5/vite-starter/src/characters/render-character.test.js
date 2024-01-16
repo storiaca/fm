f4e5ee7d446c7f9699b16539e45ee88ec14f3621
Aleksandar Ristic <storiaca@gmail.com>
2024-01-16 12:51:18 +0100

    Glob imports exercise

--- vite-v5/vite-starter/src/characters/render-character.test.js
+++ vite-v5/vite-starter/src/characters/render-character.test.js
@@ -2,6 +2,14 @@ import { test, expect } from 'vitest';
 
 import renderCharacter from './render-character';
 
+const characters = import.meta.glob('./*.json', { eager: true });
+
+for (const [path, character] of Object.entries(characters)) {
+  test(`renders ${path}`, () => {
+    const result = renderCharacter(character);
+    expect(result).toMatchSnapshot();
+  });
+}
 test('You should put your tests here', () => {
   expect(true).toBe(true);
 });
