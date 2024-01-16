236ac02d3db2cfc30357788f721727f337688f37
Aleksandar Ristic <storiaca@gmail.com>
2024-01-16 10:44:47 +0100

    Add image to page

--- vite-v5/vite-starter/src/index.js
+++ vite-v5/vite-starter/src/index.js
@@ -1,8 +1,12 @@
 //import { initializeCounter } from './counter';
-import image from './steve-after-a-workshop.jpg';
-import favicon from './favicon.png';
+//import image from './steve-after-a-workshop.jpg';
+//import favicon from './favicon.png';
+import image from './favicon.png';
 
-console.log(image, favicon);
+const img = document.createElement('img');
+img.src = image;
+
+document.querySelector('#content').appendChild(img);
 
 console.log('hello from index.js');
 
