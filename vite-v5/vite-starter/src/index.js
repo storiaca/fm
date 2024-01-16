127376655ba3490b09d745b8b780654e1e9d2b75
Aleksandar Ristic <storiaca@gmail.com>
2024-01-16 11:12:55 +0100

    Directives for images

--- vite-v5/vite-starter/src/index.js
+++ vite-v5/vite-starter/src/index.js
@@ -1,7 +1,12 @@
 //import { initializeCounter } from './counter';
-//import image from './steve-after-a-workshop.jpg';
+import webp from './steve-after-a-workshop.jpg?h=400;800&format=webp&as=metadata';
+import jpg from './steve-after-a-workshop.jpg?h=400&as=metadata';
+
 //import favicon from './favicon.png';
-import image from './favicon.png';
+//import image from './favicon.png';
+
+console.log(webp);
+console.log(jpg);
 
 const img = document.createElement('img');
 img.src = image;
