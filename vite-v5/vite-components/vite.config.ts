9102d52cdd5b779117a30a6aeada29312f0d0b78
Aleksandar Ristic <storiaca@gmail.com>
2024-01-17 10:12:37 +0100

    Injecting CSS

--- vite-v5/vite-components/vite.config.ts
+++ vite-v5/vite-components/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['src/components'] })],
+  plugins: [react(), libInjectCss(), dts({ include: ['src/components'] })],
   build: {
     copyPublicDir: false,
     lib: {
