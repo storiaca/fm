4190a03b99fbe55ca9ad5775286e91e289e502dd
Aleksandar Ristic <storiaca@gmail.com>
2024-01-30 12:08:01 +0100

    Using vitest enviroments

--- enterprise-ui-evelopment/enterprise-ui-dev/src/examples/counter/counter.test.tsx
+++ enterprise-ui-evelopment/enterprise-ui-dev/src/examples/counter/counter.test.tsx
@@ -1,8 +1,13 @@
+// @vitest-environment happy-dom
+
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />);
+  // screen.debug(document.body);
+});
 
 test.todo(
   'it should increment when the "Increment" button is pressed',
