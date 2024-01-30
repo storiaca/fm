125874b421b9c69d267f80c20a3b680af547d095
Aleksandar Ristic <storiaca@gmail.com>
2024-01-30 13:05:35 +0100

    Abstracting rendering and user events

--- enterprise-ui-evelopment/enterprise-ui-dev/src/examples/counter/counter.test.tsx
+++ enterprise-ui-evelopment/enterprise-ui-dev/src/examples/counter/counter.test.tsx
@@ -1,7 +1,8 @@
 // @vitest-environment happy-dom
 
-import { screen, render, fireEvent } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { render } from './test/utilities';
 import Counter from '.';
 
 test('it should render the component', () => {
@@ -10,12 +11,12 @@ test('it should render the component', () => {
 });
 
 test('it should increment when the "Increment" button is pressed', async () => {
-  render(<Counter />);
+  const { user } = render(<Counter />);
   const currentCount = screen.getByTestId('current-count');
   expect(currentCount).toHaveTextContent('0');
 
   const button = screen.getByRole('button', { name: 'Increment' });
-  fireEvent.click(button);
+  await user.click(button);
 
   expect(currentCount).toHaveTextContent('1');
   //screen.debug();
--- enterprise-ui-evelopment/enterprise-ui-dev/src/examples/counter/test/utilities.ts
+++ enterprise-ui-evelopment/enterprise-ui-dev/src/examples/counter/test/utilities.ts
@@ -1,1 +1,15 @@
-import '@testing-library/jest-dom/extend-expect';
+import { render as renderComponent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+export const render = (
+  ui: React.ReactElement,
+  options?: Parameters<typeof renderComponent>[1],
+) => {
+  const user = userEvent.setup();
+  const result = renderComponent(ui, options);
+
+  return {
+    ...result,
+    user,
+  };
+};
