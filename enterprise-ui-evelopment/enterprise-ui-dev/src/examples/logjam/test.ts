f9016b8fbebca79d95a31d86808918af2633b80a
Aleksandar Ristic <storiaca@gmail.com>
2024-02-02 15:36:20 +0100

    Creating mocks

--- enterprise-ui-evelopment/enterprise-ui-dev/src/examples/logjam/test.ts
+++ enterprise-ui-evelopment/enterprise-ui-dev/src/examples/logjam/test.ts
@@ -1,3 +1,12 @@
 import { test, expect, vi } from 'vitest';
+import { log } from './log';
 
-test.todo('it spies on the multiply method');
+test.todo('it spies on the multiply method', () => {
+  const mock = vi.fn();
+
+  mock();
+  mock();
+  mock();
+
+  expect(mock).toHaveBeenCalled();
+});
---
+++ enterprise-ui-evelopment/enterprise-ui-dev/src/examples/logjam/wow.test.ts
@@ -0,0 +1,18 @@
+import { test, expect, vi } from 'vitest';
+import { log } from './log';
+
+test('it spies on the multiply method', () => {
+  const mock = vi.fn((x?: string) => {
+    if (x) {
+      return x.repeat(3);
+    }
+  });
+
+  mock();
+  mock();
+  const result = mock('wow');
+
+  //expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenLastCalledWith('wow');
+  expect(result).toMatchInlineSnapshot('"wowwowwow"');
+});
