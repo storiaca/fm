a1b4dcf2079e5a4eeb0fa310ab4a434d9003a4bb
Aleksandar Ristic <storiaca@gmail.com>
2024-02-03 20:36:59 +0100

    Moving from html to htmx

--- htmx-go/fem-htmx-proj/cmd/main.go
+++ htmx-go/fem-htmx-proj/cmd/main.go
@@ -8,18 +8,18 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type Templates struct {
-	templates *template.Template
+type Template struct {
+	tmpl *template.Template
 }
 
-func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-func newTemplate() *Templates {
-	return &Templates{
-		templates: template.Must(template.ParseGlob("views/*.html")),
+func newTemplate() *Template {
+	return &Template{
+		tmpl: template.Must(template.ParseGlob("views/*.html")),
 	}
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.tmpl.ExecuteTemplate(w, name, data)
 }
 
 type Count struct {
@@ -27,13 +27,19 @@ type Count struct {
 }
 
 func main() {
+
 	e := echo.New()
-	e.Use(middleware.Logger())
 
 	count := Count{Count: 0}
+
 	e.Renderer = newTemplate()
+	e.Use(middleware.Logger())
 
 	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", count)
+	})
+
+	e.POST("/count", func(c echo.Context) error {
 		count.Count++
 		return c.Render(200, "index", count)
 	})
